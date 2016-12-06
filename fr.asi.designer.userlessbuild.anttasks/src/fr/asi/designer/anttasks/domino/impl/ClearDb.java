49cf86e8fd8f77797396a6bd2360617f0d7f45e2
Lionel HERVIER <lhervier@asi.fr>
2016-12-06 10:19:17 +0100

    clearDb can work on a databaseSet

--- fr.asi.designer.userlessbuild.anttasks/src/fr/asi/designer/anttasks/domino/impl/ClearDb.java
+++ fr.asi.designer.userlessbuild.anttasks/src/fr/asi/designer/anttasks/domino/impl/ClearDb.java
@@ -4,40 +4,27 @@ import lotus.domino.Database;
 import lotus.domino.DocumentCollection;
 import lotus.domino.NotesException;
 import lotus.domino.Session;
-import fr.asi.designer.anttasks.domino.BaseNotesTask;
+import fr.asi.designer.anttasks.domino.BaseDatabaseSetTask;
 import fr.asi.designer.anttasks.util.Utils;
 
 /**
  * Ant task to clear the content of an update site database
  * @author Lionel HERVIER
  */
-public class ClearDb extends BaseNotesTask {
+public class ClearDb extends BaseDatabaseSetTask {
 
-	/**
-	 * The server where to find the update site db
-	 */
-	private String server;
-	
-	/**
-	 * The database
-	 */
-	private String database;
-	
 	/**
 	 * The formula
 	 */
 	private String formula;
 	
-	/**
-	 * @see fr.asi.designer.anttasks.domino.BaseNotesTask#execute(lotus.domino.Session)
-	 */
 	@Override
-	public void execute(Session session) throws NotesException {
-		this.log("Clearing all content in database '" + this.server + "!!" + this.database + "'");
+	public void execute(Session session, String server, String database) throws NotesException {
+		this.log("Clearing all content in database '" + server + "!!" + database + "'");
 		Database db = null;
 		DocumentCollection coll = null;
 		try {
-			db = this.openDatabase(this.server, this.database);
+			db = this.openDatabase(server, database);
 			if( Utils.isEmpty(this.formula) )
 				coll = db.getAllDocuments();
 			else
@@ -51,20 +38,6 @@ public class ClearDb extends BaseNotesTask {
 	
 	// ===============================================================
 	
-	/**
-	 * @param server the server to set
-	 */
-	public void setServer(String server) {
-		this.server = server;
-	}
-
-	/**
-	 * @param database the database to set
-	 */
-	public void setDatabase(String database) {
-		this.database = database;
-	}
-
 	/**
 	 * @param formula the formula to set
 	 */
