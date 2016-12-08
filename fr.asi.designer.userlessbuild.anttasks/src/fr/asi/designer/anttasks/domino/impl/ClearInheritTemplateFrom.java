05bc015e9dc10c7be149f364a11bdb9f11f0d848
Lionel HERVIER <lhervier@asi.fr>
2016-12-08 13:29:22 +0100

    clearInheritTemplateFrom task

---
+++ fr.asi.designer.userlessbuild.anttasks/src/fr/asi/designer/anttasks/domino/impl/ClearInheritTemplateFrom.java
@@ -0,0 +1,25 @@
+package fr.asi.designer.anttasks.domino.impl;
+
+import lotus.domino.Document;
+import lotus.domino.NotesException;
+import fr.asi.designer.anttasks.domino.BaseDesignElementTask;
+
+/**
+ * Task to clear the template that design elements depends on
+ * @author Lionel HERVIER
+ */
+public class ClearInheritTemplateFrom extends BaseDesignElementTask {
+
+	/**
+	 * @see fr.asi.designer.anttasks.domino.BaseDesignElementTask#execute(fr.asi.designer.anttasks.domino.BaseDesignElementTask.Type, lotus.domino.Document)
+	 */
+	@Override
+	protected void execute(Type type, Document designElement) throws NotesException {
+		if( !designElement.hasItem("$Class") )
+			return;
+		log("Removing template inheritance for '" + designElement.getItemValueString("$TITLE") + "' (" + designElement.getUniversalID() + " / " + type + ")");
+		designElement.removeItem("$Class");
+		designElement.save(true, false);
+	}
+
+}
